254a5aad7f5d83e813e39f94d19c3701bc6a6f4d
Maksim Serebryakov <starkmp.work@gmail.com>
2023-09-13 03:59:57 +0300

    chore: update tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "console-error": "rgb(255, 0, 0)",
+        "console-info": "rgb(32, 33, 36)",
+        "console-warn": "rgb(92, 60, 0)",
+        "console-background-error": "rgb(255, 240, 240)",
+        "console-background-info": "transparent",
+        "console-background-warn": "rgb(255, 251, 230)",
+        "console-border-error": "rgb(255, 214, 214)",
+        "console-border-info": "rgb(243, 244, 246)",
+        "console-border-warn": "rgb(255, 245, 194)",
+      },
+    },
     container: {},
   },
   plugins: [],
